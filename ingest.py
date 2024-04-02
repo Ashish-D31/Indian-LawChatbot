8f4623594a4895809ed17debac78ff92883bedbf
VanillaHittlr <115684705+VanillaHittlr@users.noreply.github.com>
2024-04-03 01:45:48 +0530

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -1,17 +1,17 @@
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from PyPDF2 import PdfReader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.embeddings import HuggingFaceEmbeddings
 import torch
 
 def make_vectorstores():
-    text = ""
-    pdf = PdfReader('Data/IPC.pdf','Data/constitution.pdf')
-    for page in pdf.pages:
-        text += page.extract_text()
+    loader = PyPDFDirectoryLoader('Data/')
+    text = ''
+    docs = loader.load()
+    for page in docs:
+        text += page.page_content
 
     text_splitter = CharacterTextSplitter(
-        separator= '',
         chunk_size = 1024,
         chunk_overlap = 128,
         length_function = len
@@ -20,7 +20,6 @@ def make_vectorstores():
     chunks = text_splitter.split_text(text)
     
     device = ("cuda" if torch.cuda.is_available() else "cpu")
-    print(device)
     embeddings = HuggingFaceEmbeddings(model_name = 'multi-qa-mpnet-base-dot-v1' , model_kwargs = {'device': device})
 
     vectorstore = FAISS.from_texts(chunks , embeddings)
