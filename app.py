ec2e2f57589c7da82156b07eedbdc46073c651d5
Ashish Sawant <sawant.ashish013@gmail.com>
2024-05-27 21:36:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,35 +25,30 @@ def retrieval_qa_chain(llm, prompt, db):
     qa_chain = RetrievalQA.from_chain_type(
         llm=llm,
         chain_type='stuff',
-        retriever=db.as_retriever(search_kwargs={'k': 4}),
+        retriever=db.as_retriever(search_kwargs={'k': 6}),
         chain_type_kwargs={'prompt': prompt}
     )
 
     return qa_chain
 
 def qa_pipeline():
-    # Load the HuggingFace embeddings
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    embeddings = HuggingFaceEmbeddings(model_kwargs = {'device': device})
+    embeddings = HuggingFaceEmbeddings(model_name = 'multi-qa-mpnet-base-dot-v1' , model_kwargs = {'device': device})
 
-    # Load the index
     db = FAISS.load_local("vectorstore", embeddings , allow_dangerous_deserialization=True)
 
-    # Load the LLM
-    llm = LlamaCpp(model_path = path , temperature = 0.2 , n_ctx = 8196 , n_batch = 256 , n_gpu_layers = -1 , verbose = False )
+    llm = LlamaCpp(model_path = path , temperature = 0.2 , n_ctx = 2048 , n_batch = 256 , n_gpu_layers = -1 , verbose = False )
     print(path)
-    # Set the custom prompt template
     qa_prompt = set_prompt()
 
-    # Create the retrieval QA chain
     chain = retrieval_qa_chain(llm, qa_prompt, db)
     return chain
 
 def handle_user_input(user_question):
     with st.spinner("Generating response ..."):
-        response = chain(user_question)
-        response = response['result']
-        st.session_state.chat_history.append({"User": user_question, "Bot": response})
+            response = chain(user_question)
+            response = response['result']
+    st.write(bot_template.replace("{{MSG}}",response),unsafe_allow_html=True)
         
 
 st.set_page_config(page_title = "Your personal Law ChatBot", page_icon = ":bot:")
@@ -69,7 +64,7 @@ with st.sidebar:
     elif model == 'Llama2 7b (Faster)':
         path = "Models/llama-2-7b-chat.Q4_K_M.gguf"
 
-chain = qa_pipeline()
+
 
 if "chat_history" not in st.session_state:
   st.session_state.chat_history = []
@@ -77,7 +72,11 @@ if "chat_history" not in st.session_state:
 st.header("Your personal Law ChatBot :books:")
 
 user_question = st.chat_input("Ask a question :")
+with st.spinner("Loading models..."):
+    chain = qa_pipeline()
+
 if user_question:
+  st.write(user_template.replace("{{MSG}}",user_question),unsafe_allow_html=True)
   handle_user_input(user_question)
 
 for chat in st.session_state.chat_history:
